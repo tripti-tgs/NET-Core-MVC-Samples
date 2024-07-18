dd0ee0e118792e6727a70d30c21929a2e0481f4d
tripti-tgs <tripti.sharma@triveniglobalsoft.com>
2024-07-18 18:42:57 +0530

    Remove unnecessary file and code

--- Controllers/DesignerController.cs
+++ Controllers/DesignerController.cs
@@ -26,7 +26,7 @@ namespace Edit_Report_in_the_Designer.Controllers
         {
            
             var report = new StiReport();
-            var value = StiNetCoreHelper.MapPath(this, "Reports/TwoSimpleLists_2+pages.mrt");
+            var value = StiNetCoreHelper.MapPath(this, "Reports/TwoSimpleLists.mrt");
             report.Load(value);
            
         
@@ -40,20 +40,7 @@ namespace Edit_Report_in_the_Designer.Controllers
 
             return StiNetCoreDesigner.DesignerEventResult(this);
         }
-        public IActionResult OpenReport()
-        {
-            // Example: Load the report based on the reportName parameter
-            var reportPath = $"Reports/TwoSimpleLists_2+pages.mrt"; // Adjust the path as per your project structure
-            var report = new StiReport();
-            report.Load(StiNetCoreHelper.MapPath(this, reportPath));
-
-            // Apply any necessary design restrictions or modifications
-            ApplyDesignRestrictions(report);
-
-            // Return the report result
-            return StiNetCoreDesigner.GetReportResult(this, report);
-        }
-
+    
         private void ApplyDesignRestrictions(StiReport report)
         {
             foreach (StiPage page in report.Pages)
