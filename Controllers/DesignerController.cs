64ecddd91050616de3168735acf40f7734b9375a
tripti-tgs <tripti.sharma@triveniglobalsoft.com>
2024-07-18 18:15:40 +0530

    Add project files.

---
+++ Controllers/DesignerController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Mvc;
+using Stimulsoft.Report.Dictionary;
+using Stimulsoft.Report.Components;
+
+namespace Edit_Report_in_the_Designer.Controllers
+{
+    public class DesignerController : Controller
+    {
+        static DesignerController()
+        {
+            // How to Activate
+            //Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnO...";
+            //Stimulsoft.Base.StiLicense.LoadFromFile("license.key");
+            //Stimulsoft.Base.StiLicense.LoadFromStream(stream);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetReport()
+        {
+           
+            var report = new StiReport();
+            var value = StiNetCoreHelper.MapPath(this, "Reports/TwoSimpleLists_2+pages.mrt");
+            report.Load(value);
+           
+        
+                ApplyDesignRestrictions(report);
+  
+
+            return StiNetCoreDesigner.GetReportResult(this, report);
+        }
+
+        public IActionResult DesignerEvent() { 
+
+            return StiNetCoreDesigner.DesignerEventResult(this);
+        }
+        public IActionResult OpenReport()
+        {
+            // Example: Load the report based on the reportName parameter
+            var reportPath = $"Reports/TwoSimpleLists_2+pages.mrt"; // Adjust the path as per your project structure
+            var report = new StiReport();
+            report.Load(StiNetCoreHelper.MapPath(this, reportPath));
+
+            // Apply any necessary design restrictions or modifications
+            ApplyDesignRestrictions(report);
+
+            // Return the report result
+            return StiNetCoreDesigner.GetReportResult(this, report);
+        }
+
+        private void ApplyDesignRestrictions(StiReport report)
+        {
+            foreach (StiPage page in report.Pages)
+            {
+             
+                foreach (StiComponent component in page.GetComponents())
+                {
+                    component.Restrictions = 
+                        Stimulsoft.Report.Components.StiRestrictions.None;
+
+                    component.Locked = true;
+                  
+
+                }
+            }
+        }
+    }
+}
---
+++ Controllers/EditController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stimulsoft.Report.Mvc;
+using Stimulsoft.Report;
+
+namespace Edit_Report_in_the_Designer.Controllers
+{
+    public class EditController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetReport()
+        {
+            var report = new StiReport();
+            var value = StiNetCoreHelper.MapPath(this, "Reports/TwoSimpleLists_2+pages.mrt");
+            report.Load(value);
+            return StiNetCoreDesigner.GetReportResult(this, report);
+        }
+
+        public IActionResult DesignerEvent()
+        {
+            return StiNetCoreDesigner.DesignerEventResult(this);
+        }
+
+    }
+}
