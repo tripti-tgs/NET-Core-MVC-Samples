a3ab9c05e020f5730ae1d4219542f5ae96539ee6
tripti-tgs <tripti.sharma@triveniglobalsoft.com>
2024-08-13 15:33:13 +0530

    Fix: disable all option editing in report

--- Controllers/DesignerController.cs
+++ Controllers/DesignerController.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Stimulsoft.Report;
 using Stimulsoft.Report.Mvc;
-using Stimulsoft.Report.Dictionary;
 using Stimulsoft.Report.Components;
+
+using Newtonsoft.Json;
 
 namespace Edit_Report_in_the_Designer.Controllers
 {
@@ -19,40 +19,42 @@ namespace Edit_Report_in_the_Designer.Controllers
 
         public IActionResult Index()
         {
+      
             return View();
         }
-
+        [HttpPost]
         public IActionResult GetReport()
         {
-           
+
             var report = new StiReport();
             var value = StiNetCoreHelper.MapPath(this, "Reports/TwoSimpleLists.mrt");
             report.Load(value);
-           
-        
-                ApplyDesignRestrictions(report);
-  
+
+
+            ApplyDesignRestrictions(report);
+
 
             return StiNetCoreDesigner.GetReportResult(this, report);
         }
-
-        public IActionResult DesignerEvent() { 
+        [HttpPost]
+        public IActionResult DesignerEvent()
+        {
 
             return StiNetCoreDesigner.DesignerEventResult(this);
         }
-    
+
         private void ApplyDesignRestrictions(StiReport report)
         {
             foreach (StiPage page in report.Pages)
             {
-             
+
                 foreach (StiComponent component in page.GetComponents())
                 {
-                    component.Restrictions = 
+                    component.Restrictions =
                         Stimulsoft.Report.Components.StiRestrictions.None;
 
                     component.Locked = true;
-                  
+
 
                 }
             }
